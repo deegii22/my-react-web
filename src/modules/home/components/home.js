012bf0daa67389240368286d1944f50e95020381
deegii22 <deegii212@gmail.com>
2018-12-12 16:25:06 -0800

    commit

--- src/modules/home/components/home.js
+++ src/modules/home/components/home.js
@@ -3,8 +3,11 @@ import React from 'react';
 class Home extends React.Component {
     render() {
         return (
-            <div className="about">
-                Hello, I'm <span className="highlight">Munkhdelger Buyandalai.</span> <br/>I'm a front-end and full-stack developer. I am passionate about learning new technologies and skills.
+            <div>
+                <div class="home-img"><img src="img/me.jpg"/></div>
+                <div className="about">
+                    Hello, I'm <span className="highlight">Munkhdelger Buyandalai.</span> <br/>I'm a front-end and full-stack web developer focused on pixel-perfect design. Passionate about learning new web technologies and skills.
+                </div>
             </div>
         )
         
